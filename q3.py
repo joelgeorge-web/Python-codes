590089281415d620d4596c4287be06f44675b0bf
joelgerge-web <joelgeorge98765@gmail.com>
2023-08-02 09:52:15 +0530

    co2 done

--- q3.py
+++ q3.py
@@ -1,6 +1,40 @@
-#Your task is to find the number of molecules in a cup of soda which contains distilled water, carbon dioxide, and sugar.
+#Your task is to find the number of molecules in a cup of soda which contains distilled water, carbon dioxide, and glucose.
 #you have a machine thats able to count the number of atoms of carbon, hydrogen, and oxygen in a given sample.
+# water: H2O
+#caron dioxide: CO2
+#Glucose: C6H12O6
+
+#Let g be glucose, x be carbon dioxide, and w be water.
+
+#total no of c o and h is given by the equations
+# 6g + x = c ----> eqn1
+# 12g + 2w = h ----> eqn2
+# 6g + 2x + w = o ----> eqn3
+
+#From this we can find the value of g, x, and w.
+
+# eqn1 gives us x = c - 6g
+# eqn2 gives us w = (h - 12g)/2
+# Substituting in eqn3 we get g = (2o -4c - 2h)/2
 
 
-arr = list(map(int, input("Enter no of molecules: ").split()))
+
+
+arr = list(map(int, input("Enter no of c h and o: ").split()))
 print(arr)
+
+c = 0
+h = 0
+o = 0
+#CO2 molecules only if h = 0
+if arr[1] == 0:
+    if 2*arr[0] == arr[2]:
+        print("No of CO2 molecules: ",arr[0])
+    else:
+        print("invalid")
+#H2O molecules
+else:
+    if arr[1] == 2*arr[2]:
+        print("H2O: ", arr[2])
+    else:
+        print("invalid")
