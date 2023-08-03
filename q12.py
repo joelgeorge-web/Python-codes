c6f330e19e6ac765fe72d70d52f6d24d3b6cdc39
joelgerge-web <joelgeorge98765@gmail.com>
2023-08-03 12:51:37 +0530

    ans and question

--- q12.py
+++ q12.py
@@ -1,4 +1,15 @@
-#given a string find the longest palindromic substring
+#given a string find the longest palindromic sequence in it
 
 def longest_palindrome(s):
+    if len(s) == 0:
+        return 0
+    if len(s) == 1:
+        return 1
+    if s[0] == s[-1]:
+        return 2 + longest_palindrome(s[1:-1])
+    else:
+        return max(longest_palindrome(s[1:]), longest_palindrome(s[:-1]))
     
+if __name__ == "__main__":
+    s = input("Enter the string: ")
+    print(longest_palindrome(s))
